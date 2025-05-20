df75490d3b61b4d4baf00d8f4c0cbde89df5bb93
manachika <ngakanputuadityamanachika@gmail.com>
2025-05-20 20:10:25 +0700

    Create list_book.py

---
+++ list_book.py
@@ -0,0 +1,7 @@
+def list_books(books):
+    if not books:
+        print("Belum ada buku dalam katalog.")
+    else:
+        print("\nDaftar Buku:")
+        for idx, book in enumerate(books, start=1):
+            print(f"{idx}. Judul: {book['judul']} | Penulis: {book['penulis']}")
